e750284280751f780630754d5349abf5e35f1908
guilherme <oliveira.gui5@gmail.com>
2019-03-23 23:57:26 -0300

    only implemented get

--- Project1/sample_route_t.cpp
+++ Project1/sample_route_t.cpp
@@ -12,21 +12,6 @@ void sample_route_t::on_get(web::http::http_request req)
 
 }
 
-void sample_route_t::on_post(web::http::http_request req)
-{
-}
-
-void sample_route_t::on_put(web::http::http_request req)
-{
-}
-
-void sample_route_t::on_patch(web::http::http_request req)
-{
-}
-
-void sample_route_t::on_delete(web::http::http_request req)
-{
-}
 
 sample_route_t::sample_route_t(web::uri uri) :
     iroute_t(uri)
--- Project1/sample_route_t.h
+++ Project1/sample_route_t.h
@@ -6,10 +6,6 @@ class sample_route_t :
    
 public:
     void on_get(web::http::http_request req)override;
-    void on_post(web::http::http_request req)override;
-    void on_put(web::http::http_request req)override;
-    void on_patch(web::http::http_request req)override;
-    void on_delete(web::http::http_request req)override;
 
     sample_route_t(web::uri uri);
     ~sample_route_t(void) = default;
